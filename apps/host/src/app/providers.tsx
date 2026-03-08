f612da03e1243978107d7f3e90cfc31e9bdc3cd5
Meng Tian <sdusttm@gmail.com>
2026-03-08 15:09:14 -0700

    feat: switch host to use environment variables for API Url allowing Vercel deployment

--- apps/host/src/app/providers.tsx
+++ apps/host/src/app/providers.tsx
@@ -3,5 +3,7 @@
 import { TrackerProvider } from 'page-sense-library';
 
 export function Providers({ children }: { children: any }) {
-    return <TrackerProvider apiUrl="http://localhost:3001/api" apiKey="sk-ps-8ci5a1ghguda5uko66lrv9ox">{children}</TrackerProvider>;
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api";
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY || "sk-ps-8ci5a1ghguda5uko66lrv9ox";
+    return <TrackerProvider apiUrl={apiUrl} apiKey={apiKey}>{children}</TrackerProvider>;
 }
